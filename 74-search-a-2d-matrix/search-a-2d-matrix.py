f48999724ed84cc757ded2e506e1d382821bc52c
Sushrut-B <bankalgisushrut@gmail.com>
2026-06-30 08:28:02 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (95.21%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -1,13 +1,23 @@
 class Solution(object):
+    def binarySearch(self,nums,target):
+        if not nums:
+            return False
+        low,high=0,len(nums)
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid] == target:
+                return True
+            elif nums[mid]> target:
+                high=mid-1
+            else:
+                low=mid+1
+        return False
     def searchMatrix(self, mat, target):
         if not mat or not mat[0]:
             return False
         n=len(mat)
         m=len(mat[0])
         for i in range(n):
-            for j in range(m):
-                if mat[i][j] == target:
-                    return True
+            if mat[i][0] <= target <= mat[i][m-1]:
+                return self.binarySearch(mat[i],target)
         return False
-     
-        
