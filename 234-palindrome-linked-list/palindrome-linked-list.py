a44378dae8c7bffa368b49c62802c1dec97d6107
Sushrut-B <bankalgisushrut@gmail.com>
2026-07-03 10:24:03 +0530

    Time: 156 ms (11.16%) | Memory: 66.6 MB (51.11%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,44 +1,35 @@
-# Definition for singly-linked list.
-# class ListNode(object):
+# Definition of singly linked list:
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def reverseLinkedList(self, head):
-        # Initialize previous pointer as None
-        prev = None
-        
-        # Initialize current pointer as head
-        curr = head
-        
-        # Traverse the list until all nodes are processed
-        while curr is not None:
-            # Temporarily store the next node
-            next_node = curr.next
-            
-            # Reverse the link direction
-            curr.next = prev
-            
-            # Move 'prev' one step forward
-            prev = curr
-            
-            # Move 'curr' one step forward
-            curr = next_node
-        
-        # 'prev' now points to the new head after reversal
+
+class Solution:
+    def reverseLL(self,head):
+        if not head:
+            return None
+        prev=None
+        curr=head
+        while curr:
+            front=curr.next
+            curr.next=prev
+            prev=curr
+            curr=front
         return prev
 
     def isPalindrome(self, head):
-        stack=[]
-        temp=head
-        while temp:
-            stack.append(temp.val)
-            temp=temp.next
-        temp=head
-        while temp:
-            if temp.val != stack.pop():
+        if head is None or head.next is None:
+            return True
+        slow,fast=head,head
+        while fast.next and fast.next.next:
+            slow=slow.next
+            fast=fast.next.next
+        newHead=self.reverseLL(slow.next)
+        first=head
+        second=newHead
+        while second:
+            if first.val!=second.val:
                 return False
-            temp=temp.next
+            first=first.next
+            second=second.next
         return True
-
-        
